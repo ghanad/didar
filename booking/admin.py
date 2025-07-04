7d9369e4a6c988f4320f0062ea01442c382b57cf
Aghajani <aghajani.ali@gmail.com>
2025-07-04 13:28:29 +0330

    add admin item

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Room, Reservation
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'capacity', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'room', 'organizer', 'start_time', 'end_time')
+    list_filter = ('room',)
+    date_hierarchy = 'start_time'
+    search_fields = ('title', 'organizer__username')
